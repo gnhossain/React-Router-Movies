74d6b74fd557374498db42e142e88a4e4883b446
G H <gh@Gs-MacBook-Pro.local>
2019-09-17 23:46:43 -0700

    stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 const Movie = (props) => {
   const [movie, setMovie] = useState();
  
@@ -30,10 +32,11 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  //const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
+        <MovieCard movie={movie} />
+      {/* <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
@@ -48,7 +51,7 @@ const Movie = (props) => {
             {star}
           </div>
         ))}
-      </div>
+      </div> */}
       <div className="save-button">Save</div>
     </div>
   );
